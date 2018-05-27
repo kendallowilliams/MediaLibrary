84eff1a3a9df245f1bbad6a872004378be242ffa
kendallowilliams <kendallowilliams@users.noreply.github.com>
2018-05-27 12:54:13 -0400

    continuing implementation

--- MusicLibrary/MusicLibraryWebApp/src/app/app.module.ts
+++ MusicLibrary/MusicLibraryWebApp/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavMenuComponent } from './components/nav-menu/nav-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavMenuComponent
   ],
   imports: [
     BrowserModule
