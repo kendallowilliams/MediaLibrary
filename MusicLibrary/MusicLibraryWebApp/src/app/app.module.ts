f430ebdffcc1549e6dde8de1d9547567a794f713
Kendall Williams <kendallowilliams@outlook.com>
2018-06-03 18:22:34 -0400

    continuing implementation

--- MusicLibrary/MusicLibraryWebApp/src/app/app.module.ts
+++ MusicLibrary/MusicLibraryWebApp/src/app/app.module.ts
@@ -1,14 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app/app.component';
+import { NavmenuComponent } from './components/navmenu/navmenu.component';
+import { MusicComponent } from './components/music/music.component';
+import { PodcastsComponent } from './components/podcasts/podcasts.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { PlaylistsComponent } from './components/playlists/playlists.component';
+import { NowPlayingComponent } from './components/nowplaying/nowplaying.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavmenuComponent,
+    MusicComponent,
+    NowPlayingComponent,
+    PlaylistsComponent,
+    PodcastsComponent,
+    SettingsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+        { path: '', redirectTo: 'app-music', pathMatch: 'full' },
+        { path: 'app-music', component: MusicComponent },
+        { path: 'app-nowplaying', component: NowPlayingComponent },
+        { path: 'app-podcasts', component: PodcastsComponent },
+        { path: 'app-settings', component: SettingsComponent },
+        { path: 'app-playlists', component: PlaylistsComponent },
+        { path: '**', redirectTo: 'app-music' }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
