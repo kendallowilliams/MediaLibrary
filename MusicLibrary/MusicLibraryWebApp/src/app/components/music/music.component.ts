f992b76bef3f4de3dd73376f6fb5109db11223e5
Kendall Williams <kendallowilliams@outlook.com>
2018-06-06 17:19:32 -0400

    continuing implementation

--- MusicLibrary/MusicLibraryWebApp/src/app/components/music/music.component.ts
+++ MusicLibrary/MusicLibraryWebApp/src/app/components/music/music.component.ts
@@ -1,7 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { TrackService } from '../../services/track.service';
-import { Track } from '../../shared/models/track.model';
 import { Observable } from 'rxjs';
+
+import { AlbumService } from '../../services/album.service';
+import { ArtistService } from '../../services/artist.service';
+import { TrackService } from '../../services/track.service';
+
+import { Album } from '../../shared/models/album.model';
+import { Track } from '../../shared/models/track.model';
+import { Artist } from '../../shared/models/artist.model';
 
 @Component({
   selector: 'app-music',
@@ -11,14 +17,27 @@ import { Observable } from 'rxjs';
 
 export class MusicComponent implements OnInit {
   tracks: Track[];
+  artists: Artist[];
+  albums: Album[];
 
-  constructor(private trackService: TrackService) { }
+  constructor(private trackService: TrackService, private artistService: ArtistService,
+              private albumService: AlbumService) { }
 
   ngOnInit() {
     this.getTracks();
+    this.getArtists();
+    this.getAlbums();
   }
 
   getTracks(): void {
     this.trackService.getTracks().subscribe(tracks => this.tracks = tracks);
+  }
+
+  getAlbums(): void {
+    this.albumService.getAlbums().subscribe(albums => this.albums = albums);
+  }
+
+  getArtists(): void {
+    this.artistService.getArtists().subscribe(artists => this.artists = artists);
   }
 }
