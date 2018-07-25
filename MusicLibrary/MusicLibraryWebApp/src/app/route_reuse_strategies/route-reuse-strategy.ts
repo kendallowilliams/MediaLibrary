f57f2d782292ccd8548240be08910919444a2882
Kendall Williams <Kendall Williams@GAMER-PC>
2018-07-25 14:28:02 -0400

    continuing implementation

--- MusicLibrary/MusicLibraryWebApp/src/app/route_reuse_strategies/route-reuse-strategy.ts
+++ MusicLibrary/MusicLibraryWebApp/src/app/route_reuse_strategies/route-reuse-strategy.ts
@@ -1,29 +1,31 @@
-import { RouteReuseStrategy, ActivatedRouteSnapshot, DetachedRouteHandle } from "../../../node_modules/@angular/router";
+import { RouteReuseStrategy, ActivatedRouteSnapshot, DetachedRouteHandle } from '@angular/router';
 
 export class MlRouteReuseStrategy implements RouteReuseStrategy {
-    _cacheRouters: { [key: string]: any } = {};
+  _cacheRouters: { [key: string]: any } = {};
 
-    shouldDetach(route: ActivatedRouteSnapshot): boolean {
-        return true;
-    }
+  shouldDetach(route: ActivatedRouteSnapshot): boolean {
+    return true;
+  }
 
-    store(route: ActivatedRouteSnapshot, handle: DetachedRouteHandle): void {
-        this._cacheRouters[route.routeConfig.path] = {
-            snapshot: route,
-            handle: handle
-        };
-    }
+  store(route: ActivatedRouteSnapshot, handle: DetachedRouteHandle): void {
+    this._cacheRouters[route.routeConfig.path] = {
+      snapshot: route,
+      handle: handle
+    };
+  }
 
-    shouldAttach(route: ActivatedRouteSnapshot): boolean {
-        return !!route.routeConfig && !!this._cacheRouters[route.routeConfig.path];
-    }
+  shouldAttach(route: ActivatedRouteSnapshot): boolean {
+    return !!route.routeConfig && !!this._cacheRouters[route.routeConfig.path];
+  }
 
-    retrieve(route: ActivatedRouteSnapshot): DetachedRouteHandle {
-        if (!route.routeConfig || !this._cacheRouters[route.routeConfig.path]) return null;
-        return this._cacheRouters[route.routeConfig.path].handle;
+  retrieve(route: ActivatedRouteSnapshot): DetachedRouteHandle {
+    if (!route.routeConfig || !this._cacheRouters[route.routeConfig.path]) {
+      return null;
     }
-    
-    shouldReuseRoute(future: ActivatedRouteSnapshot, curr: ActivatedRouteSnapshot): boolean {
-        return future.routeConfig === curr.routeConfig;
-    }
+    return this._cacheRouters[route.routeConfig.path].handle;
+  }
+
+  shouldReuseRoute(future: ActivatedRouteSnapshot, curr: ActivatedRouteSnapshot): boolean {
+    return future.routeConfig === curr.routeConfig;
+  }
 }
