4a0cd30893185878c711b89f918ac9fae7a02319
Kendall Williams <Kendall Williams@GAMER-PC>
2018-12-26 13:11:58 -0500

    podcast updates

--- MusicLibrary/MusicLibraryWebApp/src/app/components/podcasts/podcast/podcast.component.ts
+++ MusicLibrary/MusicLibraryWebApp/src/app/components/podcasts/podcast/podcast.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Podcast } from 'src/app/shared/models/podcast.model';
 import { PodcastItem } from 'src/app/shared/models/podcast-item.model';
-import { Observable } from 'rxjs';
 import { PodcastService } from 'src/app/services/podcast.service';
-import { timeInterval } from 'rxjs/operators';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-podcast',
@@ -14,7 +14,7 @@ export class PodcastComponent implements OnInit {
   @Input() podcast: Podcast;
   protected items$: Observable<PodcastItem[]>;
 
-  constructor(private podcastService: PodcastService) { }
+  constructor(private podcastService: PodcastService, private router: Router) { }
 
   ngOnInit() {
     this.items$ = this.podcastService.getPodcastItems(this.podcast.id);
@@ -23,5 +23,9 @@ export class PodcastComponent implements OnInit {
   getNumberOfDays(date: Date): number {
     const MILLISECONDS_PER_DAY = 1000 * 60 * 60 * 24;
     return Math.floor((Date.now() - date.getTime()) / MILLISECONDS_PER_DAY);
+  }
+
+  podcastClicked(id: number) {
+    this.router.navigate(['/podcasts', id]);
   }
 }
