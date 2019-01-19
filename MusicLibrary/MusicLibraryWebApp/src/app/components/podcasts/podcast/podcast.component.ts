0a5e550f621421b9b666fcb5494ec8bb6c23a484
Kendall Williams <Kendall Williams@GAMER-PC>
2019-01-19 12:16:30 -0500

    modified last update display for podcasts

--- MusicLibrary/MusicLibraryWebApp/src/app/components/podcasts/podcast/podcast.component.ts
+++ MusicLibrary/MusicLibraryWebApp/src/app/components/podcasts/podcast/podcast.component.ts
@@ -15,9 +15,22 @@ export class PodcastComponent implements OnInit {
   ngOnInit() {
   }
 
-  getNumberOfDays(date: Date): number {
-    const MILLISECONDS_PER_DAY = 1000 * 60 * 60 * 24;
-    return Math.floor((Date.now() - date.getTime()) / MILLISECONDS_PER_DAY);
+  getLastUpdateDisplay(date: Date): string {
+    const MILLISECONDS_PER_DAY = 1000 * 60 * 60 * 24,
+          dayCount = Math.floor((Date.now() - date.getTime()) / MILLISECONDS_PER_DAY),
+          monthCount = Math.floor(dayCount / 30),
+          yearCount = Math.floor(monthCount / 12);
+    let output = '';
+
+    if (yearCount > 0) {
+      output = yearCount + (yearCount > 1 ? ' years' : ' year');
+    } else if (monthCount > 0) {
+      output = monthCount + (monthCount > 1 ? ' months' : ' month');
+    } else {
+      output = dayCount + (dayCount > 1 ? ' days' : ' day');
+    }
+
+    return output;
   }
 
   podcastClicked(id: number) {
