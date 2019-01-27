6001780eca7d6329eddbd431fdbb490bef290783
Kendall Williams <Kendall Williams@GAMER-PC>
2019-01-27 10:51:08 -0500

    additional work on the controls-related functionality

--- MusicLibrary/MusicLibraryWebApp/src/app/components/controls/controls.component.ts
+++ MusicLibrary/MusicLibraryWebApp/src/app/components/controls/controls.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Output, EventEmitter, Input, ViewChild, ElementRef } from '@angular/core';
 import { NowPlayingService } from '../../services/now-playing.service';
 import { AppService } from 'src/app/services/app.service';
-import { Observable } from 'rxjs';
+import { ReadyStateEnum } from '../music/enums/player-enum';
 
 @Component({
   selector: 'app-controls',
@@ -11,6 +11,8 @@ import { Observable } from 'rxjs';
 export class ControlsComponent implements OnInit {
   @ViewChild('player') playerRef: ElementRef<HTMLAudioElement>;
   private player: HTMLAudioElement;
+  private isPlaying: boolean;
+  private nextPreviousClicked: boolean;
 
   constructor(private nowPlayingService: NowPlayingService, private appService: AppService) {
     this.appService.controlsComponent = this;
@@ -18,26 +20,38 @@ export class ControlsComponent implements OnInit {
 
   ngOnInit() {
     this.player = this.playerRef.nativeElement;
-    this.nowPlayingService.getCurrentTrackId().subscribe(id => {
-      if (!!id && id > 0) {
-        this.player.src = 'api/Track/GetTrackFile/' + id;
-      }
-    });
+    this.nowPlayingService.getCurrentTrackId().subscribe(id => this.loadPlayer(id));
+    this.player.onplay = evt => this.isPlaying = true;
+    this.player.onpause = evt => this.isPlaying = false;
   }
 
   play(): void {
-    if (this.player.paused) {
+    if (this.player.readyState !== ReadyStateEnum.HAVE_NOTHING) {
       this.player.play();
-    } else {
-      this.player.pause();
     }
   }
 
-  playNext(): void {
-    this.nowPlayingService.setNextTrackId();
+  pause(): void {
+    this.player.pause();
   }
 
-  playPrevious(): void {
-    this.nowPlayingService.setPreviousTrackId();
+  next(): void {
+    const trackId = this.nowPlayingService.getNextTrackId();
+    this.nextPreviousClicked = true;
+    this.nowPlayingService.setCurrentTrackId(trackId);
+  }
+
+  previous(): void {
+    const trackId = this.nowPlayingService.getPreviousTrackId();
+    this.nextPreviousClicked = true;
+    this.nowPlayingService.setCurrentTrackId(trackId);
+  }
+
+  loadPlayer(id: number): void {
+    if (!!id && id > 0) {
+      this.player.src = 'api/Track/GetTrackFile/' + id;
+      if (!this.nextPreviousClicked || this.isPlaying) { this.player.play(); }
+      this.nextPreviousClicked = false;
+    }
   }
 }
--- MusicLibrary/MusicLibraryWebApp/src/app/services/now-playing.service.ts
+++ MusicLibrary/MusicLibraryWebApp/src/app/services/now-playing.service.ts
@@ -9,38 +9,34 @@ import { Guid } from 'guid-typescript';
 
 export class NowPlayingService {
   private trackSubject: BehaviorSubject<number>;
+  private podcastSubject: BehaviorSubject<number>;
   private trackIds: number[];
+  private podcastIds: number[];
   private currentTrackId: number;
+  private currentPodcastId: number;
 
   constructor() {
     this.trackSubject = new BehaviorSubject<number>(-1);
+    this.podcastSubject = new BehaviorSubject<number>(-1);
   }
 
-  setNextTrackId(): number {
+  getNextTrackId(): number {
     let nextId = null;
 
     if (!!this.trackIds) {
       const index: number = this.trackIds.indexOf(this.currentTrackId);
       nextId = this.trackIds[index + 1];
-      if (!!nextId) {
-        this.trackSubject.next(nextId);
-        this.currentTrackId = nextId;
-      }
     }
 
     return nextId;
   }
 
-  setPreviousTrackId(): number {
+  getPreviousTrackId(): number {
     let previousId = null;
 
     if (!!this.trackIds) {
       const index: number = this.trackIds.indexOf(this.currentTrackId);
       previousId = this.trackIds[index - 1];
-      if (!!previousId) {
-        this.trackSubject.next(previousId);
-        this.currentTrackId = previousId;
-      }
     }
 
     return previousId;
@@ -57,5 +53,40 @@ export class NowPlayingService {
 
   getCurrentTrackId(): Observable<number> {
     return this.trackSubject.asObservable();
+  }
+
+  getNextPodcastId(): number {
+    let nextId = null;
+
+    if (!!this.podcastIds) {
+      const index: number = this.podcastIds.indexOf(this.currentPodcastId);
+      nextId = this.podcastIds[index + 1];
+    }
+
+    return nextId;
+  }
+
+  getPreviousPodcastId(): number {
+    let previousId = null;
+
+    if (!!this.podcastIds) {
+      const index: number = this.podcastIds.indexOf(this.currentPodcastId);
+      previousId = this.podcastIds[index - 1];
+    }
+
+    return previousId;
+  }
+
+  setPodcastIds(podcastIds: number[]): void {
+      this.podcastIds = podcastIds;
+  }
+
+  setCurrentPodcastId(id: number) {
+    this.currentPodcastId = id;
+    this.podcastSubject.next(this.currentPodcastId);
+  }
+
+  getCurrentPodcastId(): Observable<number> {
+    return this.podcastSubject.asObservable();
   }
 }
