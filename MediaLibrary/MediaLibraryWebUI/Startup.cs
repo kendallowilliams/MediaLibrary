53c3dcf0be90597092256cb532305ed3401b12f8
Kendall Williams <Kendall Williams@GAMER-PC>
2019-09-15 11:19:29 -0400

    added mvc support

--- MediaLibrary/MediaLibraryWebUI/Startup.cs
+++ MediaLibrary/MediaLibraryWebUI/Startup.cs
@@ -5,6 +5,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace MediaLibraryWebUI
@@ -15,6 +18,7 @@ namespace MediaLibraryWebUI
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
