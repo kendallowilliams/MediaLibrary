d6df008f629f5c05d7bea6c5b3f1f42557fdec09
Kendall Williams <Kendall Williams@GAMER-PC>
2019-09-15 15:25:51 -0400

    updated medialibrarydal to core

--- MediaLibrary/MediaLibraryWebUI/Startup.cs
+++ MediaLibrary/MediaLibraryWebUI/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MediaLibraryDAL.DbContexts;
 using MediaLibraryDAL.Services;
 using MediaLibraryDAL.Services.Interfaces;
 using Microsoft.AspNetCore.Builder;
@@ -16,12 +17,20 @@ namespace MediaLibraryWebUI
 {
     public class Startup
     {
+        private IConfiguration config;
+
+        public Startup(IConfiguration config)
+        {
+            this.config = config;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.AddSingleton<IDataService, DataService>();
+            services.AddDbContext<MediaLibraryContext>(options => );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
