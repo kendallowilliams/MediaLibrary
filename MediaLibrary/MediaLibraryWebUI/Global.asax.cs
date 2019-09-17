12c61b1a2dee0aff7fb2515970e51edd717ac0a9
Kendall Williams <Kendall Williams@GAMER-PC>
2019-09-16 20:35:57 -0400

    continuing implementation

--- MediaLibrary/MediaLibraryWebUI/Global.asax.cs
+++ MediaLibrary/MediaLibraryWebUI/Global.asax.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace MediaLibraryWebUI
@@ -15,6 +16,7 @@ namespace MediaLibraryWebUI
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             MefConfig.Register(GlobalConfiguration.Configuration);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
