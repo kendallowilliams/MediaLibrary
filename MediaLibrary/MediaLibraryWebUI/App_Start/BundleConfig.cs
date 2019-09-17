b4a351dad4a20f7827c9f690064cb3b48ce2fbb9
Kendall Williams <Kendall Williams@GAMER-PC>
2019-09-16 20:43:21 -0400

    added popperjs

--- MediaLibrary/MediaLibraryWebUI/App_Start/BundleConfig.cs
+++ MediaLibrary/MediaLibraryWebUI/App_Start/BundleConfig.cs
@@ -16,6 +16,9 @@ namespace MediaLibraryWebUI
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/popperjs").Include(
+                      "~/Scripts/popper.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
