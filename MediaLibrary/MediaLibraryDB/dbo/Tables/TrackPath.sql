4e83ee83165a2042c61c4791e96a2f9945df7caf
Kendall Williams <kendallowilliams@outlook.com>
2019-09-19 21:29:24 -0400

    updated db datetime field

--- MediaLibrary/MediaLibraryDB/dbo/Tables/TrackPath.sql
+++ MediaLibrary/MediaLibraryDB/dbo/Tables/TrackPath.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[TrackPath] (
     [Id]             INT           IDENTITY (1, 1) NOT NULL,
     [Location]       VARCHAR (256) NOT NULL,
-    [LastScanDate] DATETIME      DEFAULT (getdate()) NOT NULL,
+    [LastScanDate] DATETIME2      DEFAULT (getdate()) NOT NULL,
     [CreateDate]    DATETIME2      DEFAULT (getdate()) NOT NULL,
     [ModifyDate]    DATETIME2      DEFAULT (getdate()) NOT NULL,
     PRIMARY KEY NONCLUSTERED ([Id] ASC)
