71f1970db222c4d887aa5914b0f737de2fb515b5
Kendall Williams <kendallowilliams@outlook.com>
2019-10-06 19:42:49 -0400

    added player

---
+++ MediaLibrary/MediaLibraryWebUI/Controllers/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using MediaLibraryWebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MediaLibraryWebUI.Controllers
+{
+    public class PlayerController : Controller
+    {
+        // GET: Player
+        public ActionResult Index()
+        {
+            PlayerViewModel model = new PlayerViewModel();
+
+            return View(model);
+        }
+    }
+}
