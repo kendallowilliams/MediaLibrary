054a00fa93b9b09ef3f715eb22bc9458487da426
Kendall Williams <kendallowilliams@outlook.com>
2019-10-27 14:59:00 -0400

    simplified model

--- MediaLibrary/MediaLibraryWebUI/Models/Configurations/PodcastConfiguration.cs
+++ MediaLibrary/MediaLibraryWebUI/Models/Configurations/PodcastConfiguration.cs
@@ -8,16 +8,12 @@ namespace MediaLibraryWebUI.Models.Configurations
 {
     public class PodcastConfiguration
     {
-        private int selectedPodcastId;
-        private PodcastPages selectedPodcastPage;
-        private PodcastSort selectedPodcastSort;
-
         public PodcastConfiguration()
         {
         }
 
-        public int SelectedPodcastId { get => selectedPodcastId; set => selectedPodcastId = value; }
-        public PodcastPages SelectedPodcastPage { get => selectedPodcastPage; set => selectedPodcastPage = value; }
-        public PodcastSort SelectedPodcastSort { get => selectedPodcastSort; set => selectedPodcastSort = value; }
+        public int SelectedPodcastId { get; set; }
+        public PodcastPages SelectedPodcastPage { get; set; }
+        public PodcastSort SelectedPodcastSort { get; set; }
     }
 }
