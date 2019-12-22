d7e3312cdb05c60326042334fdad99869a26c63f
Kendall Williams <kendallowilliams@outlook.com>
2019-12-22 01:14:45 -0500

    added tv schema to db

---
+++ MediaLibrary/MediaLibraryDB/dbo/Tables/Episode.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Episode]
+(
+	[Id] INT IDENTITY(1,1) NOT NULL , 
+    [Title] NVARCHAR(256) NOT NULL, 
+    [SeriesId] INT NOT NULL, 
+    [Season] INT NULL, 
+	[PATH] VARCHAR(256) NOT NULL,
+    [CreateDate] DATETIME2      DEFAULT (getdate()) NOT NULL,
+    [ModifyDate] DATETIME2      DEFAULT (getdate()) NOT NULL,
+    CONSTRAINT [PK_Episode] PRIMARY KEY ([Id]),
+    FOREIGN KEY ([SeriesId]) REFERENCES [dbo].[Series] ([Id]) ON DELETE CASCADE,
+)
