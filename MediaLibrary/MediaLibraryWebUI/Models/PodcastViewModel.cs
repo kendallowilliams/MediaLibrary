9f121581b6afc9019c99a4c17ecc56f9865a70aa
Kendall Williams <kendallowilliams@outlook.com>
2020-01-05 16:05:49 -0500

    removed unneeded property

--- MediaLibrary/MediaLibraryWebUI/Models/PodcastViewModel.cs
+++ MediaLibrary/MediaLibraryWebUI/Models/PodcastViewModel.cs
@@ -19,14 +19,12 @@ namespace MediaLibraryWebUI.Models
         {
             PodcastGroups = Enumerable.Empty<IGrouping<string, Podcast>>();
             PodcastItems = Enumerable.Empty<PodcastItem>();
-            DownloadedEpisodes = Enumerable.Empty<int>();
             PodcastSortItems = PodcastRepository.GetPodcastSortItems().Select(item => new SelectListItem { Text = item.Name, Value = item.Value.ToString() });
         }
 
         public IEnumerable<IGrouping<string, Podcast>> PodcastGroups { get; set; }
         public Podcast SelectedPodcast { get; set; }
         public IEnumerable<PodcastItem> PodcastItems { get; set; }
-        public IEnumerable<int> DownloadedEpisodes { get; set; }
         public IEnumerable<SelectListItem> PodcastSortItems { get; }
     }
 }
