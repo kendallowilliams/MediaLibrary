36277f13baa05b24e99bedf3739abf230c51ccb6
Kendall Williams <kendallowilliams@outlook.com>
2020-01-29 00:37:09 -0500

    minor changes

--- MediaLibrary/MediaLibraryWebUI/Repositories/HtmlControlsRepository.cs
+++ MediaLibrary/MediaLibraryWebUI/Repositories/HtmlControlsRepository.cs
@@ -15,5 +15,8 @@ namespace MediaLibraryWebUI.Repositories
         public static string HeaderPauseButtonId { get => "btn-header-pause"; }
         public static string HeaderShuffleButtonId { get => "btn-header-shuffle"; }
         public static string HeaderRepeatButtonId { get => "btn-header-repeat"; }
+        public static string PlayerVideoContainerId { get => "video-container"; }
+        public static string PlayerAudioContainerId { get => "audio-container"; }
+        public static string PlayerItemsContainerId { get => "player-items-container"; }
     }
 }
