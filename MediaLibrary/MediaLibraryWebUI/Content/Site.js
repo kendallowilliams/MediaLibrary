7f2f61c096edcfb0254ab37481b28680c0ac70ce
Kendall Williams <kendallowilliams@outlook.com>
2020-02-01 21:00:26 -0500

    minor changes

--- MediaLibrary/MediaLibraryWebUI/Content/Site.js
+++ MediaLibrary/MediaLibraryWebUI/Content/Site.js
@@ -1,8 +1,34 @@
-﻿/**
+﻿/*
  * https://www.w3schools.com/js/js_random.asp
- * @param {any} min
- * @param {any} max
+ * @param {number} min
+ * @param {number} max
  */
 function getRandomInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+/* https://www.w3schools.com/howto/howto_js_fullscreen.asp */
+function openFullscreen(obj) {
+    if (obj.requestFullscreen) {
+        obj.requestFullscreen();
+    } else if (obj.mozRequestFullScreen) { /* Firefox */
+        obj.mozRequestFullScreen();
+    } else if (obj.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+        obj.webkitRequestFullscreen();
+    } else if (obj.msRequestFullscreen) { /* IE/Edge */
+        obj.msRequestFullscreen();
+    }
+}
+
+/* https://www.w3schools.com/howto/howto_js_fullscreen.asp */
+function closeFullscreen() {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.mozCancelFullScreen) { /* Firefox */
+        document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) { /* Chrome, Safari and Opera */
+        document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { /* IE/Edge */
+        document.msExitFullscreen();
+    }
 }
