8ffa8385ff40606a8f4a5d512c3cbdfda3f42bb5
Kendall Williams <kendallowilliams@outlook.com>
2020-02-22 11:07:21 -0500

    removed default jquery-ui package/added custom jquery-ui package with only the slider widget

--- MediaLibrary/MediaLibraryWebUI/Content/Site.js
+++ MediaLibrary/MediaLibraryWebUI/Content/Site.js
@@ -1,1 +1,11 @@
-﻿
+﻿requirejs.config({
+    baseUrl: 'lib/app',
+    paths: {
+        jquery: '../jquery/jquery.min',
+        'jquery-ui': '../jquery-ui-1.12.1.custom/jquery-ui.min',
+        bootstrap: '../bootstrap/dist/js/bootstrap.bundle.min'
+    },
+    deps: ['jquery', 'jquery-ui', 'bootstrap']
+});
+
+requirejs(['app'], function (app) { });
