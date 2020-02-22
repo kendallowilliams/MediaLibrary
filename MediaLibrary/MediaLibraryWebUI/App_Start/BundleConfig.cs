0dc873d9b0d468ecce0bf9e9cb193f9325e6dfb6
Kendall Williams <kendallowilliams@outlook.com>
2020-02-22 11:22:09 -0500

    removed unneeded client libraries

--- MediaLibrary/MediaLibraryWebUI/App_Start/BundleConfig.cs
+++ MediaLibrary/MediaLibraryWebUI/App_Start/BundleConfig.cs
@@ -7,29 +7,8 @@ namespace MediaLibraryWebUI
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/polyfill").Include(
-                        "~/lib/js-polyfills/polyfill.min.js"));
-
             bundles.Add(new ScriptBundle("~/bundles/js").Include(
                         "~/Content/site.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/lib/jquery/jquery.min.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                        "~/lib/jqueryui/jquery-ui.min.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/marquee").Include(
-                        "~/lib/jQuery.Marquee/jquery.marquee.min.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/lib/modernizr/modernizr.min.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/lib/bootstrap/dist/js/bootstrap.min.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/popperjs").Include(
-                      "~/lib/popper.js/umd/popper.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/fontawesome").Include(
                       "~/lib/font-awesome/js/all.min.js"));
