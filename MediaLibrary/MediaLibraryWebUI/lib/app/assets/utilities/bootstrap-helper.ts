ef9828ee8f20f26d80bd224959bf4d44e5911e3a
Kendall Williams <kendallowilliams@outlook.com>
2020-02-21 23:00:00 -0500

    minor change

--- MediaLibrary/MediaLibraryWebUI/lib/app/assets/utilities/bootstrap-helper.ts
+++ MediaLibrary/MediaLibraryWebUI/lib/app/assets/utilities/bootstrap-helper.ts
@@ -1,6 +1,4 @@
-﻿//import * as Bootstrap from 'bootstrap';
-
-export function loadTooltips(element: HTMLElement): void {
+﻿export function loadTooltips(element: HTMLElement): void {
     if (element) /*then*/ $(element).find('*[data-tooltip="tooltip"]').tooltip({ trigger: 'hover' });
 }
 
