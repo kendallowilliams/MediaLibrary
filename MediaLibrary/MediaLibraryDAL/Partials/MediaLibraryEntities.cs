fd9438689092bab566c899a5aaccc4269ad1f96c
Kendall Williams <kendallowilliams@outlook.com>
2020-04-17 09:18:59 -0400

    added dbcontext partial

---
+++ MediaLibrary/MediaLibraryDAL/Partials/MediaLibraryEntities.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaLibraryDAL.DbContexts
+{
+    public partial class MediaLibraryEntities
+    {
+        public MediaLibraryEntities(string nameOrConnectionString) : base(nameOrConnectionString) { }
+    }
+}
