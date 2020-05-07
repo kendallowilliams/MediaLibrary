27188c310c2c929016b86d5aa723133fa7773bca
Kendall Williams <kendallowilliams@outlook.com>
2020-05-07 01:53:45 -0400

    fixed playlist/podcast serialization issue

--- MediaLibrary/MediaLibraryDAL/Partials/Podcast.cs
+++ MediaLibrary/MediaLibraryDAL/Partials/Podcast.cs
@@ -1,5 +1,6 @@
 ﻿using MediaLibraryDAL.Models;
 using MediaLibraryDAL.Models.Interfaces;
+using MediaLibraryDAL.Partials.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,7 +10,7 @@ using static MediaLibraryDAL.Enums.TransactionEnums;
 
 namespace MediaLibraryDAL.DbContexts
 {
-    public partial class Podcast: IDataModel
+    public partial class Podcast: IDataModel, IPodcast
     {
         public Podcast(string title, string url, string imageUrl, string description, string author)
         {
