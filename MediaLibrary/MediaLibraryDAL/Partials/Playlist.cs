a0e67b72e33124b777233d5795a5772a15c5736e
Kendall Williams <kendallowilliams@outlook.com>
2020-05-07 20:43:02 -0400

    removed unneeded partials

--- MediaLibrary/MediaLibraryDAL/Partials/Playlist.cs
+++ MediaLibrary/MediaLibraryDAL/Partials/Playlist.cs
@@ -1,6 +1,5 @@
 ﻿using MediaLibraryDAL.Models;
 using MediaLibraryDAL.Models.Interfaces;
-using MediaLibraryDAL.Partials.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,7 +9,7 @@ using static MediaLibraryDAL.Enums.TransactionEnums;
 
 namespace MediaLibraryDAL.DbContexts
 {
-    public partial class Playlist: IDataModel, IPlaylist
+    public partial class Playlist: IDataModel
     {
         public Playlist(string name)
         {
--- MediaLibrary/MediaLibraryDAL/Partials/Podcast.cs
+++ MediaLibrary/MediaLibraryDAL/Partials/Podcast.cs
@@ -1,6 +1,5 @@
 ﻿using MediaLibraryDAL.Models;
 using MediaLibraryDAL.Models.Interfaces;
-using MediaLibraryDAL.Partials.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,7 +9,7 @@ using static MediaLibraryDAL.Enums.TransactionEnums;
 
 namespace MediaLibraryDAL.DbContexts
 {
-    public partial class Podcast: IDataModel, IPodcast
+    public partial class Podcast: IDataModel
     {
         public Podcast(string title, string url, string imageUrl, string description, string author)
         {
