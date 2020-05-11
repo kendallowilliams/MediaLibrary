2a59d1ac6d4a6c8b76eb07325aa7c70e1040058b
Kendall Williams <kendallowilliams@outlook.com>
2020-05-11 11:47:29 -0400

    fixed issue with playlist item counts

--- MediaLibrary/MediaLibraryWebUI/Services/PlaylistUIService.cs
+++ MediaLibrary/MediaLibraryWebUI/Services/PlaylistUIService.cs
@@ -37,7 +37,7 @@ namespace MediaLibraryWebUI.Services
                                                                                                     playlist => playlist.PlaylistEpisodes);
             var playlistTypeSortMappings = PlaylistRepository.GetPlaylistTypePlaylistSortMappings();
 
-            playlists = playlists.Concat(await GetSystemPlaylists());
+            playlists = playlists.Concat(await GetSystemPlaylists(true, true));
            
             foreach(var group in playlists.GroupBy(playlist => playlist.Type))
             {
